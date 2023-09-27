d4ec586419de6c33db18c43997f832a85e2bb04e
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-27 18:22:14 -0400

    Added async function to fetch user data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 // ~~~~~~~~~~~~~~~~~ GLOBAL VARIABLES ~~~~~~~~~~~~~~~~~
+// targets div with class = overview
 const overview = document.querySelector(".overview");
+// specifies GitHub user
 const username = "MeganWallace";
+
+const getUser = async function(){
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const data = await response.json();
+  console.log(data);
+}
+
